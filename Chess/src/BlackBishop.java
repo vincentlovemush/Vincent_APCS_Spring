1404a076d773fee66d5caa1d8420ea1828ba3239
vincentlovemush <vincentyangyang@gmail.com>
2017-05-12 09:51:32 -0700

    fixed bishop and did rook
    yay next is en pessante

--- Chess/src/BlackBishop.java
+++ Chess/src/BlackBishop.java
@@ -9,7 +9,37 @@ public class BlackBishop extends BlackPiece {
 		super(a);
 		}
 	public boolean validMove(int moveX, int moveY, int prevX, int prevY) {
-	
+		int x2=moveX-prevX;
+		int y2=moveY-prevY;
+		if(Math.abs(x2)!=Math.abs(y2)){
+			System.out.println("bad move!!"+Math.abs(x2)+" "+ Math.abs(y2));
+			return false;
+		}
+		if(prevX>moveX&&prevY>moveY)
+			for(int i=1;i<Math.abs(x2);i++)
+				if(board[prevX-i][prevY-i] instanceof BlackPiece ||board[prevX-i][prevY-i] instanceof WhitePiece){
+					System.out.println("a");
+					return false;
+				}
+		if(prevX>moveX&&prevY<moveY)
+			for(int i=1;i<Math.abs(x2);i++)
+				if(board[prevX-i][prevY+i] instanceof BlackPiece ||board[prevX-i][prevY+i] instanceof WhitePiece){
+					System.out.println("b");
+					return false;
+				}
+		if(prevX<moveX&&prevY>moveY)
+			for(int i=1;i<Math.abs(x2);i++)
+				if(board[prevX+i][prevY-i] instanceof BlackPiece ||board[prevX+i][prevY-i] instanceof WhitePiece){
+					System.out.println("c");
+					return false;
+				}
+		if(prevX<moveX&&prevY<moveY)
+			for(int i=1;i<Math.abs(x2);i++)
+				if(board[moveX+i][moveY+i] instanceof BlackPiece ||board[moveX+i][moveY+i] instanceof WhitePiece){
+					System.out.println("d");
+					return false;
+				}
+
 		return true;
 	}
 	public boolean isWhite(){
--- Chess/src/WhiteRook.java
+++ Chess/src/WhiteRook.java
@@ -7,8 +7,44 @@ public class WhiteRook extends WhitePiece {
 	private ImageIcon wRook= new ImageIcon("C:/Users/APCS2/Desktop/Vincent_APCS_Spring/Chess/rook white.png");
 	public WhiteRook(Chesspiece [][] a){
 		super(a);
-		}
+	}
 	public boolean validMove(int moveX, int moveY, int prevX, int prevY) {	
+		int x2=Math.abs(moveX-prevX);
+		int y2=Math.abs(moveY-prevY);
+		if(x2!=0 && y2!=0)
+			return false;
+		if(y2==0){
+			if (prevX-moveX>0){
+				for (int i=prevX-1;i>moveX;i--)
+				{
+					if (board[i][moveY] instanceof BlackPiece || board[i][moveY] instanceof WhitePiece)
+						return false;
+				}
+			}
+			else if (prevX-moveX<0){
+				for (int i=prevX+1;i<moveX;i++)
+				{
+					if (board[i][moveY] instanceof BlackPiece || board[i][moveY] instanceof WhitePiece)
+						return false;
+				}
+			}
+		}
+		else if(x2==0){
+			if (prevY-moveY>0){
+				for (int i=prevY-1;i>moveY;i--)
+				{
+					if (board[moveX][i] instanceof BlackPiece || board[moveX][i] instanceof WhitePiece)
+						return false;
+				}
+			}
+			else if (prevY-moveY<0){
+				for (int i=prevY+1;i<moveY;i++)
+				{
+					if (board[moveX][i] instanceof BlackPiece || board[moveX][i] instanceof WhitePiece)
+						return false;
+				}
+			}
+		}
 		return true;
 	}
 	public boolean isWhite(){
@@ -20,6 +56,6 @@ public class WhiteRook extends WhitePiece {
 	@Override
 	public void moved() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
